2a25dc7a78d36a45e5320a70f9e604eba393c98f
Shaurya <shaurya.sharma1404@gmail.com>
2025-06-03 02:47:44 -0700

    Polyfills

---
+++ JS_Questions/polyfills.js
@@ -0,0 +1,49 @@
+// A Polyfill is a piece of code which provides a specific modern functionality of JavScript in older versions of browsers
+
+// Q. Write a Polyfill for the .map() method
+
+if(!Array.prototype.myMap) {
+    Array.prototype.myMap = function (callback) {
+        let arr = new Array(this.length);
+        for(i = 0; i < this.length; i++){
+            arr[i] = callback(this[i], i);
+        }
+        return arr
+    }
+}
+
+let arr = [1, 2, 3, 4, 5]
+let mappedArr = arr.myMap((value, index) => value * 2);
+console.log(mappedArr);
+
+console.log("\n");
+
+// Q. Write a Polyfill for the .forEach() method
+
+if(!Array.prototype.myForEach) {
+    Array.prototype.myForEach = function (callback) {
+        for(i = 0; i < this.length; i++){
+            callback(this[i], i);
+        }
+    }
+}
+
+let forEachArr = arr.myForEach((value, index) => {console.log(value * 2);})
+console.log("\n");
+
+// Q. Write a Polyfill for the .reduce() method
+
+if(!Array.prototype.myReduce) {
+    Array.prototype.myReduce = function (callback, init) {
+            let acc = init || this[0] // if init != 0, null, undefined, then use init. else, use this[0]
+            const startIdx = (acc == init ? 1 : 0)
+            for(i = startIdx; i < this.length; i++){
+                acc = callback(acc, this[i]);
+            }
+            return acc
+        
+    }
+}
+
+let reduced = arr.myReduce((acc, curr) => acc * curr);
+console.log(reduced);
