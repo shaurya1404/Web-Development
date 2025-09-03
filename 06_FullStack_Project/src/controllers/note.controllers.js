71949501cef709b1a1168b54575e734224616db9
Shaurya Sharma <shaurya.sharma1404@gmail.com>
2025-09-03 20:33:18 +0530

    Added permissions to projects based on user roles

--- 06_FullStack_Project/src/controllers/note.controllers.js
+++ 06_FullStack_Project/src/controllers/note.controllers.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { UserRoleEnums } from "../utils/constants.js"
-import { validateProjectPermissions } from "../middleware/auth.middleware.js";
+import { isLoggedIn, validateProjectPermissions } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/:projectId")
-    .get(validateProjectPermissions([UserRoleEnums.ADMIN, UserRoleEnums.MEMBER]), getNotes) // Both user and admin (only those who are part of the project) can perform a "get" action on the project, i.e, anyone can read the its contents
-    .post(validateProjectPermissions([UserRoleEnums.ADMIN]), createNotes) // Giving permission only to project admin to perform "post" action
+    .get(isLoggedIn, validateProjectPermissions([UserRoleEnums.ADMIN, UserRoleEnums.MEMBER]), getNotes) // Both user and admin (only those who are part of the project) can perform a "get" action on the project, i.e, anyone can read the its contents
+    .post(isLoggedIn, validateProjectPermissions([UserRoleEnums.ADMIN]), createNotes) // Giving permission only to project admin to perform "post" action
 
 export default router 
